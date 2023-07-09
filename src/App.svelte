b7d90d977d59fecfee14e3f8d0373cb0dde4d33c
Roy <royrao2333@gmail.com>
2023-07-09 11:16:38 +0800

    chore: Update README

--- src/App.svelte
+++ src/App.svelte
@@ -20,9 +20,11 @@
   <Counter id="0" />
 
   <p class="mx-auto my-4 max-w-xs leading-5 sm:max-w-none">
-    Visit <a class="text-red-600 hover:underline" href="https://svelte.dev"
-      >svelte.dev</a
-    > to learn how to build Svelte apps.
+    Visit
+    <a class="text-red-600 hover:underline" href="https://svelte.dev">
+      svelte.dev
+    </a>
+    to learn how to build Svelte apps.
   </p>
 
   <p class="mx-auto my-4 max-w-xs leading-5 sm:max-w-none">
@@ -31,25 +33,29 @@
   </p>
 
   <div class="p-2 text-red-500">
-    <a href="https://svelte.dev/" target="_blank" class="p-2 hover:underline"
-      >Svelte</a
-    >
+    <a href="https://svelte.dev/" target="_blank" class="p-2 hover:underline">
+      Svelte
+    </a>
     +
     <a
       href="https://tailwindcss.com/"
       target="_blank"
-      class="p-2 hover:underline">Tailwind CSS</a
+      class="p-2 hover:underline"
     >
+      Tailwind CSS
+    </a>
     +
     <a
       href="https://www.typescriptlang.org/"
       target="_blank"
-      class="p-2 hover:underline">TypeScript</a
+      class="p-2 hover:underline"
     >
+      TypeScript
+    </a>
     +
-    <a href="https://vitejs.dev/" target="_blank" class="p-2 hover:underline"
-      >Vite</a
-    >
+    <a href="https://vitejs.dev/" target="_blank" class="p-2 hover:underline">
+      Vite
+    </a>
   </div>
 </main>
 
